5b38aad5dc324476abf790458cd366d16940c38d
Matteo Perozzi <mat.perozzi@github.com>
2019-06-13 17:23:09 +0200

    Modifica 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("ciao");
     }
 }
